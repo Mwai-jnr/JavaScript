a96a67c4b4d51fe21ee1e350b85a1ab28e06bc0b
Kiria-ini Mission Hospital <mwaivictor07@yahoo.com>
2021-07-22 13:04:57 +0300

    #1

--- Guides/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
+++ Guides/complete-javascript-course-master/01-Fundamentals-Part-1/starter/script.js
@@ -1,5 +1,60 @@
-let js = 'amazing';
-if (js == 'amazing') alert ('javaScript is FUN');
+// let js = 'amazing';
+// console.log(40 + 8 + 23 -10);
 
-40 + 8 + 23 -10; 
-console.log(40 + 8 + 23 -10);
+// // value is a piece of data 'jonas' this is a value.
+// console.log('jonas');
+// console.log('23');
+
+// //  value smallest unit of information can be stored in variable and be reused.
+// let firstName = "Matilda";
+
+// // fisrtname is a variable which store the value jonas.
+// console.log (firstName);
+// console.log (firstName);
+// console.log (firstName);
+
+// // variables should be descriptive i.e. like below.
+// let myFirstJob = "programmer";
+// let myCurrentJob = "teacher";
+
+// let job1 = "programmer";
+// let job2 = "teacher";
+
+// Assignment_01 values and variables
+
+/*
+let country = 'Kenya';
+let continent = 'Africa';
+let population = '20,000,000';
+
+console.log (country);
+console.log (continent);
+console.log(population);
+*/
+
+/* 
+Data types:- Number,Strings,Boolean,Undefined,Null,symbol,Bigint.
+*/
+/*
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Jonas');
+
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
+
+// undefined data_type
+let year;
+console.log (year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+// bug or error declared as an object which is wrong
+console.log(typeof null);
+*/
